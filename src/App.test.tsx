f3a81376d6945847ea8b2347e90388c294a30213
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-06-24 09:52:28 -0400

    Minor changes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+test("renders text", () => {
+  const { getByText } = render(<App />);
+  const element = getByText(/Text/i);
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
+  expect(element).toHaveTextContent("Text");
 });
